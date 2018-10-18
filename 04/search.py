3646b6ebf0c4c7e0dac266104795775e6edc3c07
Martin <martin.k738@gmail.com>
2018-10-18 20:21:53 +0200

    backbone of search

--- 04/search.py
+++ 04/search.py
@@ -8,9 +8,20 @@ def main():
     db = db_connector.connect_db()
     cursor = db.cursor()
     rows_with_name = get_all_rows_with_name(name, cursor)
+    result_dict = {}
+    for row in rows_with_name:
+        list_of_prints = get_list_of_prints_for_name(row, cursor)
+        result_dict[row[3]] = list_of_prints
+    json.dump(result_dict, sys.stdout, indent=4, ensure_ascii=False)
 
 
 def get_all_rows_with_name(name, cursor):
     name_for_select = "%"+name+"%"
     cursor.execute("SELECT * FROM person where name LIKE ?", (name_for_select,))
     return cursor.fetchall()
+
+
+def get_list_of_prints_for_name(row, cursor):
+    name = row[3]
+    result_list = []
+    return result_list
