50daeb105fe4a3b1c1fbef73a9ad0efc6aeecec0
Martin <martin.k738@gmail.com>
2018-10-23 23:03:43 +0200

    parsing finished

--- 05/eqn.py
+++ 05/eqn.py
@@ -12,36 +12,42 @@ def main():
     for line in file:
         splitted_equations.append(re.findall(r"(-? ?[0-9a-zA-Z]+)", line))
     for equation in splitted_equations:
-        res_list = []
-        for i, component in enumerate(equation):
-            if is_minus(component[0]):
-                res_list.append(get_number(component[1:], variables, i)*(-1))
-            else:
-                res_list.append(get_number(component, variables, i))
+        res_list = get_equation_as_numbers(equation, variables)
         matrix.append(res_list[:-1])
         results.append(res_list[-1])
-    print(matrix, "###", results, "###", variables)
+    print(matrix, "###", results, "###", variables)  # check of parsing -- delete before final push
 
 
-def is_minus(char):
-    return char == "-"
+def get_equation_as_numbers(eq, variables):
+    res_list = []
+    d = {}
+    for i, component in enumerate(eq):
+        if component[0] == "-":
+            t = get_info(component[1:])
+            is_negative = True
+        else:
+            t = get_info(component)
+            is_negative = False
+        if t[0] is not None:
+            var = t[0].group(1)
+            if var not in variables:
+                variables.append(var)
+            d[var] = actual_number(t[1])*(-1) if is_negative else actual_number(t[1])
+        else:
+            res_list.insert(i, actual_number(t[1]))
+    for i, var in enumerate(variables):
+        if var in d.keys():
+            res_list.insert(i, d[var])
+        else:
+            res_list.insert(i, 0)
+    return res_list
 
 
-def get_number(component, variables, index):
+def get_info(component):
     c = component.strip()
     number = re.match(r"([0-9]+)", c)
     variable = re.match(r".*([a-zA-Z])", c)
-    if variable is not None:
-        var = variable.group(1)
-        if var in variables:
-            if variables[index] == var:
-                return actual_number(number)
-            #TODO
-        else:
-            variables.append(var)
-            return actual_number(number)
-    else:
-        return actual_number(number)
+    return variable, number
 
 
 def actual_number(number):
