09d34948a0e090b00c3af2bde37085f4f3c11f00
Martin <martin.k738@gmail.com>
2018-11-20 12:34:39 +0100

    student.py done?

--- 08/student.py
+++ 08/student.py
@@ -3,6 +3,7 @@ import sys
 import numpy
 import json
 import datetime
+import math
 
 
 def main():
@@ -17,24 +18,17 @@ def get_mean_median(data):
 def get_passed_count(data):
     passed = 0
     for d in data:
-        if int(d) > 0:
+        if float(d) > 0:
             passed += 1
     return passed
 
 
 def get_slope(data):
-    extra_length = len("/NN")*(-1)
-    grouped_data = data.groupby(data.keys().map(lambda x: x[:extra_length])).sum()
-    data_with_days_from_start_of_semester = grouped_data.rename(lambda x: update_key(x))
-    print(data_with_days_from_start_of_semester)
-    return None
-    # y = numpy.array(data.values[0])
-    # print(y)
-    # x = numpy.array(range(0, len(data.values[0])))
-    # print(x)
-    # A = numpy.vstack([x, [0]*len(x)]).T
-    # print(A)
-    # return numpy.polyfit(data.values[0], [0]*len(data.values[0]), 1)
+    cumulated_data = data.cumsum()
+    x = numpy.array(cumulated_data.keys())
+    y = numpy.array(cumulated_data.values)
+    a = numpy.vstack([x, numpy.zeros(len(x))]).T
+    return numpy.linalg.lstsq(a, y, 1)[0][0]
 
 
 def update_key(key):
@@ -50,14 +44,39 @@ def get_data(data):
         student_data = data.drop(columns="student").mean()
     else:
         student_data = data[data['student'] == int(student)].drop(columns="student").mean()
-    mean_median = get_mean_median(student_data)
+    data_with_days_from_start_of_semester = get_data_with_days_from_start_of_semester(student_data)
+    data_grouped_by_exercises = get_data_grouped_by_exercises(student_data)
+    mean_median = get_mean_median(data_grouped_by_exercises)
+    slope = get_slope(data_with_days_from_start_of_semester)
     return {
         "mean": mean_median[0],
         "median": mean_median[1],
-        "total": student_data.sum(),
-        "passed": get_passed_count(student_data),
-        "regression slope": get_slope(student_data)
+        "total": data_with_days_from_start_of_semester.sum(),
+        "passed": get_passed_count(data_grouped_by_exercises),
+        "regression slope": slope,
+        "date 16": get_date_from_days(16/slope),
+        "date 20": get_date_from_days(20/slope)
     }
+
+
+def get_data_grouped_by_exercises(data):
+    extra_length = len("YYYY-MM-DD/")
+    return data.groupby(data.keys().map(lambda x: x[extra_length:])).sum()
+
+
+def get_data_with_days_from_start_of_semester(data):
+    extra_length = len("/NN") * (-1)
+    grouped_data = data.groupby(data.keys().map(lambda x: x[:extra_length])).sum()
+    data_with_days_from_start_of_semester = grouped_data.rename(lambda x: update_key(x))
+    return data_with_days_from_start_of_semester
+
+
+def get_date_from_days(days_from_start_of_semester):
+    start_of_semester = datetime.date(2018, 9, 17)
+    timedelta = datetime.timedelta(days=days_from_start_of_semester)
+    date = start_of_semester+timedelta
+    print("DATE - DELTA:", date-timedelta)
+    return "%d-%02d-%02d" % (date.year, date.month, date.day)
 
 
 main()
